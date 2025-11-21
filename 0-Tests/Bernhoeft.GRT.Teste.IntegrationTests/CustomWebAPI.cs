abe1e5d1314ffabc38321767c6e20559c8d38105
Ueverton Carmo <uevertonandrade@gmail.com>
2025-11-21 11:02:50 -0300

    Clean up comments in CustomWebAPI class
    Removed unnecessary comment about configuring test services.

--- 0-Tests/Bernhoeft.GRT.Teste.IntegrationTests/CustomWebAPI.cs
+++ 0-Tests/Bernhoeft.GRT.Teste.IntegrationTests/CustomWebAPI.cs
@@ -5,6 +5,6 @@ public class CustomWebAPI : WebApplicationFactory<Program>
 {
     /*Normalmente aqui eu sobrescreveria o método ConfigureWebHost para configurar serviços de teste, como
     por exemplo um banco de dados em memória ou mocks de serviços externos. Mas como a API já utiliza um banco
-    de dados SQLite em memória para os testes de integração, não há necessidade de adicionar outro. 
+    de dados em memória para os testes de integração, não há necessidade de adicionar outro. 
     */
 }
